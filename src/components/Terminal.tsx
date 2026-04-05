53cc74188e9f873ba8cb2dadf78748ae2bed7fc2
Caleb Li <caleb.jesseli@gmail.com>
2026-04-05 02:35:01 -0400

    fix to highlighting

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -185,13 +185,14 @@ export default function Terminal() {
   }
 
   function focusInput() {
+    if (window.getSelection()?.toString()) return;
     inputRef.current?.focus();
   }
 
-  // add max-w-5xl mx-auto in first class component to make it center
+  // add/remove max-w-5xl mx-auto in first class component to make it center
   return (
     <div
-      class="flex flex-col h-screen w-full px-4 py-6 cursor-text"
+      class="flex flex-col h-screen w-full max-w-5xl mx-auto px-4 py-6 cursor-text"
       onClick={focusInput}
     >
       {/* Output history */}
